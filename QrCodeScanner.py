02ac9d63c85aca725cfdad568c876149baa6bfa7
Horgun <saviobola@gmail.com>
2016-10-05 16:08:38 -0400

    1 - Foi retirado a verificação para guardar N quadrados encontrados, pegando tudo que encontrar. 2 - Foi criado uma classe Square para definir as coordenadas de um quadrado e seu tamanho. 3 - Foi criado um método para pegar as informações importantes do array do numpy e colocar na nossa estrutura (classe Square)

--- QrCodeScanner.py
+++ QrCodeScanner.py
@@ -6,24 +6,59 @@ import time
 
 TAM = 150
 
+class Square:
+	x1 = x2 = y1 = y2 = None
+	width = None
+
+	def __init__(self, x1, x2, y1, y2):
+		self.x1 = x1
+		self.x2 = x2
+		self.y1 = y1
+		self.y2 = y2
+		self.width = x2 - x1
+
+	def __str__(self):
+		string = "X1 = " + str(self.x1) + " Y1 = " + str(self.y1) + "\nX2 = " + str(self.x2) + " Y2 = " + str(self.y2) + "\nW = " + str(self.width)
+		return string + "\n"
+
+
 def canClose():
 	ch = 0xFF & cv.waitKey(1)
 	return ch == 27
 
-def getBorders(img, qtdd, dots = 4):
+def getSquare(approx):
+	X = []
+	Y = []
+	for i in range(4):
+		X.append(int(approx[i][0][0]))
+		Y.append(int(approx[i][0][1]))
+	s = Square(min(X), max(X), min(Y), max(Y))
+	return s
+
+def getSquares(img):
 	imgBorders = img.copy()
 	contours, hierarchy = cv.findContours(img,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)#encontra os contornos
-	contours = sorted(contours, key = cv.contourArea, reverse = True)[:10]#ordena os contornos pelo tamanho da área, do maior pro menor, o 10 maiores
+	contours = sorted(contours, key = cv.contourArea, reverse = True)[:20]#ordena os contornos pelo tamanho da área, do maior pro menor, o 10 maiores
 	borders = []
+	squares = []
 	#filtrar
 	for c in contours:
 		peri = cv.arcLength(c, True)
 		approx = cv.approxPolyDP(c, 0.018 * peri, True)
-		if len(approx) == dots :#se o contorno tem X pontos
+		if len(approx) == 4 :#se o contorno tem X pontos
+			s = getSquare(approx)
+			squares.append(s)
 			borders.append(approx)#adiciona os contornos na lista
-			if len(borders) == qtdd:#se já tiver X elementos na lista
-				break
-	return borders, imgBorders;
+	return borders, imgBorders, squares;
+
+def getPositionMarkers(squares):
+	module3 = []
+	module5 = []
+	module7 = []
+
+
+	return 0
+	
 
 def GetCode():
 	cap = cv.VideoCapture(0)
@@ -45,13 +80,14 @@ def GetCode():
 		imgCaixa = img[meioH-TAM:meioH+TAM, meioV-TAM:meioV+TAM]#pega o pedaço da imagem que será analisado
 		imgCaixa = cv.cvtColor(imgCaixa, cv.COLOR_BGR2GRAY)#converte para escala de cinza
 		ret, imgCaixa = cv.threshold(imgCaixa, 127, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)#realiza o limiar
-		kernel = np.ones((1,5),np.uint8)#cria o kernel
+		kernel = np.ones((3,3),np.uint8)#cria o kernel
 		imgCaixa = cv.morphologyEx(imgCaixa, cv.MORPH_OPEN, kernel)#Faz erosion e dilation
 		imgEscura = img.copy()#imagem para fazer a parte escura q é exibida
 		cv.convertScaleAbs(img, imgEscura, 1.0/2, 0)#faz a imagem ficar escura
 		imgEscura[meioH-TAM:meioH+TAM,meioV-TAM:meioV+TAM] = img[meioH-TAM:meioH+TAM,meioV-TAM:meioV+TAM]#recupera apenas a parte que deve ficar clara na exibição e coloca na imagem escura
 		
-		screenCnt, imgBorders = getBorders(imgCaixa, 4)
+		screenCnt, imgBorders, squares = getSquares(imgCaixa)
+		algumaCoisa = getPositionMarkers(squares)
 		img2 = img[meioH-TAM:meioH+TAM,meioV-TAM:meioV+TAM]
 		cv.drawContours(img2, screenCnt, -1, (0,255,0), 1)#desenha os contornos
 		imgEscura = cv.flip(imgEscura, 1)
